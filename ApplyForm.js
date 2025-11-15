859fde01aabcbee88e4d47b176ae3106576bf6c7
Divya Kumari <167309679+rchika@users.noreply.github.com>
2025-11-15 18:20:50 +0530

    Create ApplyForm.js

---
+++ ApplyForm.js
@@ -0,0 +1,14 @@
+
+
+const mongoose = require("mongoose");
+
+const ApplyFormSchema = new mongoose.Schema({
+  fullname: String,
+  mobile: String,
+  email: String,
+  loanType: String,
+  message: String,
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model("ApplyForm", ApplyFormSchema);
